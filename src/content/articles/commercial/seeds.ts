02727ae60fa18ceeb56723b5b26f06632b0be9cd
neoenox <neoenox@gmail.com>
2026-09-23 09:53:41 +0900

    Register Logicool M350s vs M650 commercial seed

--- src/content/articles/commercial/seeds.ts
+++ src/content/articles/commercial/seeds.ts
@@ -80,6 +80,7 @@ import { jblFlip7VsCharge6Seed } from "./jbl-flip-7-vs-charge-6";
 import { ankerA121aVsA2688Seed } from "./anker-a121a-vs-a2688";
 import { ankerA1664VsA1654Seed } from "./anker-a1664-vs-a1654";
 import { shokzOpenfit2PlusVsOpenfit2Seed } from "./shokz-openfit-2-plus-vs-openfit-2";
+import { logicoolPebbleM350sVsM650Seed } from "./logicool-pebble-m350s-vs-m650";
 
 export const commercialArticleSeeds: readonly CommercialArticleSeed[] = [
   ankerNanoA1638VsA1256Seed,
@@ -90,6 +91,7 @@ export const commercialArticleSeeds: readonly CommercialArticleSeed[] = [
   ankerA121aVsA2688Seed,
   ankerA1664VsA1654Seed,
   shokzOpenfit2PlusVsOpenfit2Seed,
+  logicoolPebbleM350sVsM650Seed,
   ankerNanoPowerBankVsZoloA1688Seed,
   soundcoreLiberty5ProVsLiberty4ProSeed,
   ...kakakuSeptember2026Seeds,
